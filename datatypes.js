35a57d3b2ccdf5bc772ac260bd6467111b02d4c1
Siddami <93784429+Siddami@users.noreply.github.com>
2022-02-11 19:01:22 +0100

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -8,37 +8,37 @@ console. log (typeof null); //null
 
 
 //Declare four variables without assigning values
-let firstname;// firstname variable
+let firstName;// firstname variable
 
 let Age;//age varable
 
-let Issingle;// is single variable
+let isSingle;// is single variable
 
 let gender;//gender variable
 
 //Declare four variables with assigned values
 
-middlename = 'damilola'//my middle name
+middleName = 'damilola'//my middle name
 
-single_age = 23 //my age
+singleAge = 23 //my age
 
 const gravity = 9.81 // normal value for gravity (here i declared and added a value to a const variable)
 
-Issingle= true //the boolean for Issingle
+isSingle= true //the boolean for Issingle
 
 
 
 //Declare variables to store your first name, last name, marital status, country and age in multiple lines
 
 //i declared the variables here
-let first_name = 'siddiqat'
-let marital_status = 'single'
+let firstName = 'siddiqat'
+let maritalStatus = 'single'
 let country = 'Nigeria'
 let age = 23
 
 //i logged them into seperat elines here
-console.log (first_name);
-console.log (marital_status);
+console.log (firstName);
+console.log (maritalStatus);
 console.log (country);
 console.log (age);
 
@@ -48,7 +48,7 @@ console.log (age);
 
 //since first name , lastname,martal status, country and age have already been delared above i would just put them in the same line and not decleare them again
 
-console.log (first_name, marital_status , country , age);
+console.log (firstName, maritalStatus , country , age);
 
 //Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
 
